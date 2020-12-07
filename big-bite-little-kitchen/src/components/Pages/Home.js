f7b33fa54aa5e06fa325c0bbe4004854277e5679
Shayan Gagan <shayangagan@Shayans-MacBook-Pro.local>
2020-12-07 15:39:23 -0600

    changes to json to help deloyment attempt

--- big-bite-little-kitchen/src/components/Pages/Home.js
+++ big-bite-little-kitchen/src/components/Pages/Home.js
@@ -5,14 +5,10 @@ function Home () {
     <div className="Home">
       <br />
       <br />
+      <h1>Welcome</h1>
       <br />
       <br />
-      <br />
-      <br />
-      <h1>Welcome.</h1>
-      <br />
-      <br />
-      <h3>BIG BITE, little kitchen welcomes all cooks and hopes to share in the childhood belief that with the right tools, a positive mindset and quality ingredients, anybody can cook!</h3>
+      <h3>BIG BITE, little kitchen welcomes all cooks and hopes to share in the belief that with the right tools, a positive mindset and quality ingredients, anybody can cook!</h3>
       <br />
       <br />
       <br />
